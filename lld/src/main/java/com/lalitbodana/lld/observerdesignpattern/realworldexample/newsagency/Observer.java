9d193054b4723fe80fec1b08fa9ca63cec81ef14
lalitbodana236 <lalitbodana236@gmail.com>
2024-12-16 06:46:15 +0530

    Observer Design Pattern Real World Example NEW Agency

---
+++ lld/src/main/java/com/lalitbodana/lld/observerdesignpattern/realworldexample/newsagency/Observer.java
@@ -0,0 +1,5 @@
+package com.lalitbodana.lld.observerdesignpattern.realworldexample.newsagency;
+
+public interface Observer {
+    void update(String news);
+}
---
+++ lld/src/main/java/com/lalitbodana/lld/observerdesignpattern/realworldexample/newsagency/ObserverPatternExample.java
@@ -0,0 +1,20 @@
+package com.lalitbodana.lld.observerdesignpattern.realworldexample.newsagency;
+
+import com.lalitbodana.lld.observerdesignpattern.realworldexample.newsagency.impl.EmailSubscriber;
+import com.lalitbodana.lld.observerdesignpattern.realworldexample.newsagency.impl.MobileSubscriber;
+
+public class ObserverPatternExample {
+    public static void main(String[] args) {
+        NewsAgency newsAgency = new NewsAgency();
+
+        Observer emailSubscriber = new EmailSubscriber("Alice");
+        Observer mobileSubscriber = new MobileSubscriber("Bob");
+
+        newsAgency.addObserver(emailSubscriber);
+        newsAgency.addObserver(mobileSubscriber);
+
+        newsAgency.publishNews("Breaking News: Java is awesome!");
+        newsAgency.removeObserver(mobileSubscriber);
+        newsAgency.publishNews("Update: Observer Pattern made easy!");
+    }
+}
