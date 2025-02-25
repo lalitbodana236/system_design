e2ee8b6e824977626482a28bdd3e76a8d2d06da9
lalitbodana236 <lalitbodana236@gmail.com>
2025-02-25 21:28:47 +0530

    Data Structure Hashing Questions

--- lld/src/main/java/com/lalitbodana/lld/ds/recursion/subsequence/GeneratePermutation.java
+++ lld/src/main/java/com/lalitbodana/lld/ds/recursion/subsequence/GeneratePermutation.java
@@ -1,5 +1,8 @@
 package com.lalitbodana.lld.ds.recursion.subsequence;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GeneratePermutation {
     public static void main(String[] args) {
 
@@ -19,5 +22,41 @@ public class GeneratePermutation {
         }
 
 
+    }
+
+    public static void backtrack(String str, String ans, boolean[] visited) {
+        if (ans.length() == str.length()) {
+            System.out.println(ans);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                backtrack(str, ans + str.charAt(i), visited);
+                visited[i] = false; // Backtrack
+            }
+        }
+    }
+
+    public static void permute(int[] arr, int idx, List<List<Integer>> ans) {
+        if (idx == arr.length) {
+            List<Integer> temp = new ArrayList<>();
+            for (int num : arr) temp.add(num);
+            ans.add(temp); // Store a new copy to avoid modification issues
+            return;
+        }
+
+        for (int i = idx; i < arr.length; i++) {
+            swap(arr, idx, i);
+            permute(arr, idx + 1, ans);
+            swap(arr, idx, i); // Backtrack
+        }
+    }
+
+    public static void swap(int[] arr, int s, int e) {
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
     }
 }
