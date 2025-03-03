72ae7f4d5fdef532b4b4ae79b4f718f5a914c86c
lalitbodana236 <lalitbodana236@gmail.com>
2025-03-03 22:49:45 +0530

    Data Structure Stack Questions

---
+++ lld/src/main/java/com/lalitbodana/lld/ds/queue/CustomQueue.java
@@ -0,0 +1,19 @@
+package com.lalitbodana.lld.ds.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CustomQueue {
+    public static void main(String[] args) {
+        Queue q = new LinkedList();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.poll());
+        System.out.println("queue " + q);
+        System.out.println(q.peek());
+        System.out.println("queue" + q);
+
+
+    }
+}
---
+++ lld/src/main/java/com/lalitbodana/lld/ds/stack/EvaluateExpression.java
@@ -0,0 +1,23 @@
+package com.lalitbodana.lld.ds.stack;
+
+import java.util.Stack;
+
+public class EvaluateExpression {
+    public static void main(String[] args) {
+        solve(new String[]{"2", "1", "+", "3", "*"});
+    }
+
+    public static void solve(String[] arr) {
+        Stack<Integer> stack = new Stack();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != "+" && arr[i] != "-" && arr[i] != "*") {
+                stack.push(Integer.parseInt(arr[i]));
+            } else {
+                Integer num1 = stack.pop();
+                Integer num2 = stack.pop();
+                //  stack.push(num1 + arr[i]+num2);
+
+            }
+        }
+    }
+}
