876eb25511e7f3eba39cf2855f4e800bb207cdd5
lalitbodana236 <lalitbodana236@gmail.com>
2025-03-18 06:25:37 +0530

    LinkedList Question

--- lld/src/main/java/com/lalitbodana/lld/observerdesignpattern/store/Stock.java
+++ lld/src/main/java/com/lalitbodana/lld/observerdesignpattern/store/Stock.java
@@ -3,6 +3,7 @@ package com.lalitbodana.lld.observerdesignpattern.store;
 import com.lalitbodana.lld.observerdesignpattern.observable.IphoneObservalbleImpl;
 import com.lalitbodana.lld.observerdesignpattern.observable.StocksObservable;
 import com.lalitbodana.lld.observerdesignpattern.observer.EmailAlertObserverImpl;
+import com.lalitbodana.lld.observerdesignpattern.observer.MobileAlertObserverImpl;
 import com.lalitbodana.lld.observerdesignpattern.observer.NotificationAlertObserver;
 
 public class Stock {
@@ -12,9 +13,11 @@ public class Stock {
         NotificationAlertObserver observer1 = new EmailAlertObserverImpl("xyz@gmail.com", iphoneStocksObservable);
         NotificationAlertObserver observer2 = new EmailAlertObserverImpl("abc@gmail.com", iphoneStocksObservable);
         NotificationAlertObserver observer3 = new EmailAlertObserverImpl("uvz@gmail.com", iphoneStocksObservable);
+        NotificationAlertObserver observer4 = new MobileAlertObserverImpl("9098024125", iphoneStocksObservable);
         iphoneStocksObservable.add(observer1);
         iphoneStocksObservable.add(observer2);
         iphoneStocksObservable.add(observer3);
+        iphoneStocksObservable.add(observer4);
 
         iphoneStocksObservable.setStockCount(10);
     }
