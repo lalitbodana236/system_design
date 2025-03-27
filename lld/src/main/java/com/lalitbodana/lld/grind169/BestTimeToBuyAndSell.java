5138c1e39fc6a3e0bd73c702ed8263bc3fb9940e
lalitbodana236 <lalitbodana236@gmail.com>
2025-03-27 22:37:08 +0530

    Tree Questions

---
+++ lld/src/main/java/com/lalitbodana/lld/grind169/BestTimeToBuyAndSell.java
@@ -0,0 +1,23 @@
+package com.lalitbodana.lld.grind169;
+
+public class BestTimeToBuyAndSell {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int buy = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < buy) {
+                buy = prices[i];
+            } else
+                maxProfit = Math.max(prices[i] - buy, maxProfit);
+
+
+        }
+
+        return maxProfit;
+    }
+}
