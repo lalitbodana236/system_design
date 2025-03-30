5468af5409e82a378710d50a5fa8cfd8c7c1c2f1
lalitbodana236 <lalitbodana236@gmail.com>
2025-03-30 11:13:44 +0530

    Tree Questions

---
+++ lld/src/main/java/com/lalitbodana/lld/grind169/LongestPalindromicSubstring.java
@@ -0,0 +1,39 @@
+package com.lalitbodana.lld.grind169;
+
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        String s = "babad";
+        LongestPalindromicSubstring lps = new LongestPalindromicSubstring();
+        System.out.println(lps.longestPalindrome(s));
+    }
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+
+        StringBuilder longestPal = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            StringBuilder pal1 = expandAroundCenter(s, i, i);
+            StringBuilder pal2 = expandAroundCenter(s, i, i + 1);
+
+            if (pal1.length() > longestPal.length()) {
+                longestPal = pal1;
+            }
+            if (pal2.length() > longestPal.length()) {
+                longestPal = pal2;
+            }
+        }
+
+        return longestPal.toString();
+    }
+
+    private StringBuilder expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return new StringBuilder(s.substring(left + 1, right));
+    }
+}
---
+++ lld/src/main/java/com/lalitbodana/lld/hackerrank/MinAndMaxSum.java
@@ -0,0 +1,31 @@
+package com.lalitbodana.lld.hackerrank;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MinAndMaxSum {
+    public static void main(String[] args) {
+        miniMaxSum(Arrays.asList(1, 3, 5, 7, 9));
+    }
+
+    public static void miniMaxSum(List<Integer> arr) {
+        long totalSum = 0;
+        int minVal = Integer.MAX_VALUE;
+        int maxVal = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            totalSum += num;
+            minVal = Math.min(minVal, num);
+            maxVal = Math.max(maxVal, num);
+        }
+
+        long minSum = totalSum - maxVal;
+        long maxSum = totalSum - minVal;
+
+
+        System.out.println(minSum + " " + maxSum);
+
+
+    }
+
+}
