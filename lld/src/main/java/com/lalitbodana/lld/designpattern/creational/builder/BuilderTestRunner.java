d03679593ac9f002bad1edc07cecd694376607d7
lalitbodana236 <lalitbodana236@gmail.com>
2025-07-27 21:58:34 +0530

    Creational Design Patter

--- lld/src/main/java/com/lalitbodana/lld/designpattern/creational/builder/BuilderTestRunner.java
+++ lld/src/main/java/com/lalitbodana/lld/designpattern/creational/builder/BuilderTestRunner.java
@@ -2,10 +2,7 @@ package com.lalitbodana.lld.designpattern.creational.builder;
 
 public class BuilderTestRunner {
     public static void main(String[] args) {
-        String sb = "This" +
-                            "is" +
-                            "example";
-        sb;
+        
         QueryBuilder builder = new QueryBuilder();
         Query query = builder
                               .select(new String[]{"name", "age"}, "users")
